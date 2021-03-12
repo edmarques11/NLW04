ab108599c46abee005fef331886b25a6106b1859
edmarques11 <edmarquessantosdelima@gmail.com>
2021-03-11 22:32:59 -0300

    fourth class nodejs 37:55

---
+++ src/services/SendEmailService.ts
@@ -0,0 +1,25 @@
+import nodemailer, { Transporter } from "nodemailer";
+
+class SendEmailService {
+  private client: Transporter;
+
+  constructor() {
+    nodemailer.createTestAccount().then((account) => {
+      const transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+
+      this.client = transporter;
+    });
+  }
+
+  async execute() {}
+}
+
+export { SendEmailService };
