ff8e4bd98edb92a5601845caf80ff004b22e1e76
edmarques11 <edmarquessantosdelima@gmail.com>
2021-04-04 00:12:21 -0300

    fifth class nodejs nlw04

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
+import { AppError } from "../errors/AppError";
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (error) {
+      throw new AppError(error);
+    }
 
     const usersRepository = getCustomRepository(UsersRepository);
     const userAlreadExists = await usersRepository.findOne({
@@ -12,9 +25,7 @@ class UserController {
     });
 
     if (userAlreadExists) {
-      return response.status(400).json({
-        error: "User alread exists!",
-      });
+      throw new AppError("User alread exists!");
     }
     const user = usersRepository.create({
       name,
